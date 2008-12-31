834b685ad4078eb5d9f17e76296ccbafaced2f81
Brian Reily <breily@gmail.com>
2008-12-31 02:22:48 -0500

    coming along...,  see todo for next

--- app.py
+++ app.py
@@ -1,43 +1,50 @@
 from juno import *
 
 @get(['/', '/index'])
-def index(request):
+def index(web):
     body = '''
         <html>
             <head> <title>index</title> </head>
             <body> index </body>
         </html>'''
-    return JunoResponse(body=body, status='200')
+    return JunoResponse(body=body, status=200)
 
 @get('/wildcard/*:name/')
-def hmmm(request, name):
-    return str(name)
+def wildcard(web, name): return str(name)
+
+@get('/x/:a/:b/:c/')
+def x(web, a, b, c):
+    return str(a) + str(b) + str(c)
 
 @get('/query/')
-def query(request):
-    response = 'Query string: %s' %request.QUERY_STRING
-    return response
+def query(web):
+    return 'Query: %s' %web.input()
 
-@attach('/form/')
-def form(request):
-    body = '''
+@route('/form/')
+def form(web):
+    return '''
         <form action='/post_test' name='form_test' method='post'>
             <input type='text' name='word' />   <br />
             <input type='text' name='two' />    <br />
             <input type='text' name='three' />  <br />
             <input type='submit' value='test' />
         </form>'''
-    return body
 
 @post('/post_test/')
-def post_test(request):
-    return 'value of "word": %s' %request.post('word')
+def post_test(web): return 'value of "word": %s' %web.post('word')
 
 @get('/json_test/')
-def json(request):
-    j = JunoResponse('', '200')
+def json(web):
+    j = JunoResponse()
     j['Content-Type'] = 'text/json'
-    j.append('''{'main': ['a': 5, 'b': 1]}''')
-    return j
+    return j.append('''{'main': ['a': 5, 'b': 1]}''')
+
+@get('/ua/')
+def user_agent(web):
+    return 'User Agent: %s' %web.headers['HTTP_USER_AGENT']
+
+@get('/implicit/')
+def implicit(web):
+    respond(body='Hi there')
 
 if __name__ == '__main__': run()
--- juno.py
+++ juno.py
@@ -2,18 +2,22 @@ import SocketServer
 
 class Juno:
     def __init__(self, config=None):
-        self.urls = []
+        """Takes an optional configuration dictionary. """
+        self.routes = []
         self.config = {
                 'log':      True,
                 'mode':     'scgi',
                 'scgi_port': 6969,
+                '404_template': '',
+                '500_template': '',
+                'name': 'JunoApp',
                 }
-        if config is not None:        
-            self.config.update(config)
+        if config is not None: self.config.update(config)
 
     def run(self):
+        """Runs the Juno hub, in the set mode (default now is scgi). """
         if self.config['mode'] == 'scgi':
-            SCGIRequestHandler.process = self.process_request
+            SCGIRequestHandler.process = self.request
             server = SocketServer.ThreadingTCPServer(
                 ('', self.config['scgi_port']), SCGIRequestHandler)
             try:
@@ -28,35 +32,44 @@ class Juno:
             print 'error: only scgi is supported now'
             print 'exiting juno...'
 
-    def process_request(self, request):
-        if self.config['log']: 
-            print request.raw
-        for url in self.urls:
-            if url.match(request):
-                response = url.call()
-                if type(response) == str:
-                    response = JunoResponse(response, '200')
+    def request(self, request):
+        """Called when a request is received, routes a url to its request. 
+        Returns a string, currently set as a JunoResponse.render()."""
+        if self.config['log']: print request.raw
+        for route in self.routes:
+            if route.match(request):
+                response = route.dispatch()
+                if response is None:
+                    global _response
+                    response = _response
+                if not isinstance(response, JunoResponse):
+                    response = JunoResponse(body=str(response))
                 return response.render()    
-        return JunoResponse('page not found', '404').render()        
+        return JunoResponse(body='page not found', status=404).render()        
 
-    def attach(self, url, func, method):
+    def route(self, url, func, method='*'):
+        """Attaches a view to a url or list of urls, for a given function. """
         if type(url) == str:
-            self.urls.append(JunoURL(url, func, method))
+            self.routes.append(JunoRoute(url, func, method))
         else:
             for u in url:
-                self.urls.append(JunoURL(u, func, method))
+                self.routes.append(JunoRoute(u, func, method))
 
-class JunoURL:
+    def __repr__(self):
+        return '<Juno: %s>' %self.config['name']
+
+class JunoRoute:
     """Modifiers:
         '*' - Matches previous characted repeated; by itself, matches anything
-        '&' - Matches any non-numeric character
-        '#' - Matches any numeric character
         ':' - Separator to capture results into names
 
+        Considering: 
+            '@' - Matches any non-numeric character
+            '#' - Matches any numeric character
+
         Example: '/hello/*:name/' will catch '/hello/brian/', and name = 'brian'
-        Example: '/#*/' will match '/1234/', but not '/h123/'
     """    
-    modifiers = ['*', '@', '#', ':']
+    modifiers = ['*', ':']
     def __init__(self, url, func, method):
         if url[0] != '/': url = '/' + url
         if url[-1] != '/': url += '/'
@@ -67,19 +80,20 @@ class JunoURL:
         self.params = []
     
     def match(self, request):
+        """Matches a request uri to this url object. """
         req_parts = request.DOCUMENT_URI.split('/')
         for a, b in zip(req_parts, self.url_parts):
             # if no modifiers, just check string equality
-            if not self.has_modifiers(b): 
+            if not self.has_modifiers(b):
                 if a == b: continue
                 else: return False
-            # temporary code to see if it works
+            # just matches '/*:name/' type constructs
             if ':' in b:
-                if b.split(':')[0] == '*':
-                    self.params.append((b.split(':')[1], a))
+                x, y = b.split(':')
+                if x in ['*', '']:
+                    self.params.append((y, a))
                     continue
-            # end temporary code
-            return False #right?
+            return False
         if self.method == '*' or self.method == request.REQUEST_METHOD:
             self.request = request
             return True
@@ -92,17 +106,17 @@ class JunoURL:
             elif i == 0 or b[i - 1] != '\'': return True
         return False
             
-    def call(self):
-        # this dict() call won't work if some params are named and others aren't
-        if self.params:
-            return self.func(self.request, **dict(self.params))
-        return self.func(self.request)    
+    def dispatch(self):
+        if self.params: return self.func(self.request, **dict(self.params))
+        return self.func(self.request)
 
     def __repr__(self):
-        return '<JunoURL: %s %s - %s()>' %(self.method, self.url, self.func.__name__)
+        return '<JunoRoute: %s %s - %s()>' %(self.method, self.url, self.func.__name__)
 
 class JunoRequest:
     def __init__(self, request):
+        self.headers = request
+        del self.headers['POST_DATA']
         if request['DOCUMENT_URI'][-1] != '/':
             request['DOCUMENT_URI'] += '/'
         self.raw = request
@@ -113,22 +127,30 @@ class JunoRequest:
         self.parse_query_string('POST_DATA')
     
     def parse_query_string(self, header):
+        """Adds elements of the query string to ['input'].  If the key
+        already appears, make it point to a list of the values. """
         q = self.raw[header] if header in self.raw else None
         if not q: return
         q = q.split('&')
         for param in q:
-            tmp = param.split('=')
-            self.raw['input'][tmp[0]] = tmp[1]
+            k, v = param.split('=', 1)
+            if k in self.raw['input'].keys():
+                if isinstance(self.raw['input'][k], list):
+                    self.raw['input'][k].append(v)
+                else: self.raw['input'][k] = [self.raw['input'][k], v]
+            else:    
+                self.raw['input'][k] = v
         
     def __getattr__(self, attr):
         if attr in self.keys():
-            return self.raw[attr]    
-        raise NameError()    
+            return self.raw[attr]
+        raise NameError
         
-    def input(self, arg):
+    def input(self, arg=None):
+        if arg is None: return self.raw['input']
         if self.raw['input'].has_key(arg):
             return self.raw['input'][arg]
-        return None    
+        return None
 
     def __getitem__(self, key): return self.raw[key]
     def __setitem__(self, key, val): self.raw[key] = val
@@ -138,35 +160,53 @@ class JunoRequest:
     def __len__(self): return len(self.raw)
     def __contains(self, key): return key in self.raw
 
+    def __repr__(self):
+        return '<JunoRequest: %s>' %self.location
+
 class JunoResponse:
-    status_codes = {'200': 'OK', '302': 'Found', '404': 'Not Found'}
-    def __init__(self, body, status='200', content_type='text/html', headers=None):
-        self.body = body
-        self.status = status
-        self.headers = {
-            'Content-Length': len(body),
-            'Content-Type': content_type,
-            }
-        headers = headers if headers else {}
-        self.headers.update(headers)
+    status_codes = {200: 'OK', 302: 'Found', 404: 'Not Found'}
+    def __init__(self, config=None, **kwargs):
+        self.config = {
+            'body': '',
+            'status': 200,
+            'headers': {
+                'Content-Type': 'text/html',
+            },
+        }
+        if config is None: config = {}
+        self.config.update(config)
+        self.config.update(kwargs)
+        self.config['headers']['Content-Length'] = len(self.config['body'])
     
     def append(self, text):
-        self.body += text
-        self.headers['Content-Length'] = len(self.body)
+        self.config['body'] += text
+        self.config['headers']['Content-Length'] = len(self.config['body'])
+        return self
+
+    def __iadd__(self, text):
+        return self.append(text)
 
     def render(self):
-        response = 'HTTP/1.1 %s %s\r\n' %(self.status, self.status_codes[self.status])
-        for header, val in self.headers.items():
+        response = 'HTTP/1.1 %s %s\r\n' %(self.config['status'],
+            self.status_codes[self.config['status']])
+        for header, val in self.config['headers'].items():
             response += ': '.join((header, str(val))) + '\r\n'
         response += '\r\n'
-        response += '%s' %self.body
+        response += '%s' %self.config['body']
         return response
 
     def header(self, header, value):
-        self.headers[header] = value
+        self.config['headers'][header] = value
+        return self
     
     def __setitem__(self, header, value): self.header(header, value)
-    def __getitem__(self, header): return self.headers[header]
+    def __getitem__(self, header): return self.config['headers'][header]
+
+    def __getattr__(self, attr):
+        return self.config[attr]
+
+    def __repr__(self):
+        return '<JunoResponse: %s %s>' %(self.status, self.status_codes[self.status])
 
 class SCGIRequestHandler(SocketServer.BaseRequestHandler):
     def finish(self):
@@ -175,7 +215,7 @@ class SCGIRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         data = self.request.recv(1024)
-        if data: 
+        if data:
             request_dict = self.build_scgi_dict(data)
             self.request.send(self.process(request_dict))
         self.request.close()
@@ -189,7 +229,7 @@ class SCGIRequestHandler(SocketServer.BaseRequestHandler):
         data_len, data = data.split(':', 1)
         count = 0
         data_list = data.split(chr(0))
-        data_dict = { 'len': data_len }
+        data_dict = {}
         while data_list[count][0] != ',':
             data_dict[data_list[count]] = data_list[count + 1]
             count += 2
@@ -199,6 +239,8 @@ class SCGIRequestHandler(SocketServer.BaseRequestHandler):
     
     def process(self, data): return ''
 
+class NoViewsAssigned(Exception): pass
+
 _hub = None
 
 def init(config=None):
@@ -206,17 +248,37 @@ def init(config=None):
     _hub = _hub if _hub else Juno(config)
 
 def run():
-    _hub.run()
+    if _hub: _hub.run()
+    else: raise NoViewsAssigned('No urls attached to Juno')
 
-def attach(url, method='*'):
+def route(url, method='*'):
     global _hub
     if _hub is None: init()
     def wrap(f):
-        _hub.attach(url, f, method)
+        _hub.route(url, f, method)
     return wrap
 
-def post(url):   return attach(url, 'post')
-def get(url):    return attach(url, 'get')
-def head(url):   return attach(url, 'head')
-def put(url):    return attach(url, 'put')
-def delete(url): return attach(url, 'attach')
+def post(url):   return route(url, 'post')
+def get(url):    return route(url, 'get')
+def head(url):   return route(url, 'head')
+def put(url):    return route(url, 'put')
+def delete(url): return route(url, 'delete')
+
+_response = None
+
+def redirect(url):
+    global _response
+    _response = JunoResponse(status=302, body='Location: %s' %url)
+    return _response
+
+def respond(**kwargs):
+    global _response
+    _response = JunoResponse(**kwargs)
+    return _response
+
+def notfound(template=None):
+    """ Add the 404 template"""
+    if template is None: 
+        global _hub
+        template = _hub.config['404_template'] 
+    return JunoResponse(status=404)
