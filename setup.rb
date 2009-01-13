9c98432a38e1dc35a94f44b242bcc0db9a410c1a
Brian Reily <breily@gmail.com>
2009-01-13 08:19:14 -0500

    added setup/install script

---
+++ setup.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'find'
+require 'ftools'
+
+INSTALL_DIR = '/usr/local/lib/python2.5/site-packages/'
+
+puts "** copying juno.py to #{INSTALL_DIR}..."
+puts "** overwriting..." if File.exists?(INSTALL_DIR + 'juno.py')
+File.copy 'juno.py', INSTALL_DIR
+
+puts "** copying handlers.py to #{INSTALL_DIR}..."
+puts "** overwriting..." if File.exists?(INSTALL_DIR + 'handlers.py')
+File.copy 'handlers.py', INSTALL_DIR
+
+puts "** setup complete"
