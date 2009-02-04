57f49d9f3b6b2be9d6f72caf64e4892c3c374676
Brian Reily <breily@gmail.com>
2009-02-04 13:28:26 -0500

    * Removed setup.rb script * Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import sys
+
+try:
+    import jinja2
+except:
+    print '** Jinja2 templates are required by Juno'
+    print '** Download from: http://pypi.python.org/pypi/Jinja2'
+    sys.exit()
+
+try:
+    import sqlalchemy
+except:
+    print '** SQLAlchemy is required by Juno'
+    print '** Download from: http://sqlalchemy.org/download.html'
+    sys.exit()
+
+setup(name         = 'juno',
+      description  = 'A lightweight Python web framework',
+      author       = 'Brian Reily',
+      author_email = 'brian@brianreily.com',
+      url          = 'http://brianreily.com/project/juno/',
+      version      = '0.1',
+      py_modules   = ['juno'],
+     )
