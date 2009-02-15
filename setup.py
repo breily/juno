bc94d2625466332e3d84d5f592610ae2dc1a3889
Brian Reily <brian@brianreily.com>
2009-02-14 23:25:01 -0500

    * Changed setup.py to not require Jinja2.

--- setup.py
+++ setup.py
@@ -2,14 +2,6 @@ from distutils.core import setup
 import sys
 
 try:
-    import jinja2
-except:
-    print '** Jinja2 templates are required by Juno'
-    print '** Download from: http://pypi.python.org/pypi/Jinja2'
-    print '** Or run: `easy_install Jinja2`'
-    sys.exit()
-
-try:
     import sqlalchemy
 except:
     print '** SQLAlchemy is required by Juno'
@@ -23,6 +15,14 @@ except:
     print '** To use SCGI, Juno requires flup'
     print '** Download from: http://trac.saddi.com/flup/'
     print "** If you don't want SCGI, disregard this message"
+
+try:
+    import jinja2
+except:
+    print '** Jinja2 templates are the default templates for Juno'
+    print '** Download from: http://pypi.python.org/pypi/Jinja2'
+    print '** Or run: `easy_install Jinja2`'
+    print '** Again, Jinja2 is not required.'
 
 setup(name         = 'juno',
       description  = 'A lightweight Python web framework',
