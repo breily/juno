cc1939e8d15552903516a522011edb768e15a8a1
Brian Reily <brian@brianreily.com>
2009-02-16 22:41:22 -0500

    * Added models example; * Added TODO items

---
+++ doc/examples/models.py
@@ -0,0 +1,26 @@
+# This example has errors
+
+from juno import *
+
+Person = model('Person', name='str', views='int')
+
+@route('/makeperson/*:name/')
+def makeperson(web, name):
+    exist = Person.find().filter(Person.name==name)
+    if len(exist) != 0:
+        return 'That person already exists'
+    p = Person(name=name, views=0)
+    p.save()
+    return 'Person created'
+
+@route('/getperson/*:name/')
+def getperson(web, name):
+    exist = Person.find().filter(Person.name==name).all()
+    if len(exist) == 0:
+        return 'That person does not exist'
+    p = exist[0]
+    p.views += 1
+    p.save()
+    return 'You have viewed this person %s time(s)' % p.views
+
+run()
