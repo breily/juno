21f50f69921e82bc5d336f4567b330b4fc4034fb
Brian Reily <brian@brianreily.com>
2009-03-02 02:08:47 -0500

    * Commented and cleaned up code. * Added a TOTO for the tutorial.

--- doc/tutorial/main.py
+++ doc/tutorial/main.py
@@ -1,90 +1,50 @@
 import juno
-import httplib, urlparse # For checking URL validity
 
 # Initialize Juno here
 #  - db_location: urls.db => Create an SQLite database called urls.db
 #  - use_debugger: True   => Use the Werkzeug provided debugger
-juno.init({
-    'db_location': 'urls.db',
-    'use_debugger': True,
-    })
+juno.init({'db_location': 'urls.db',
+           'use_debugger': True,
+          })
 
 # Create a model to store URLs
 #  - Juno stores it under the name 'URL'
-#  - address and tiny will be VARCHAR fields in your database
+#  - 'address' and 'tiny' will be VARCHAR fields in your database
 URL = juno.model('URL', address='string', tiny='string')
 
 # Set up our first controller
 #  - This function will be called when you receive any request for '/'
 @juno.route('/')
 def index(web):
+    # Render our main template
     juno.template('index.html', {'urls': URL.find().all()})
 
+# Set up our preview controller
+#  - Displays the actual URL for the 'tiny' version
 @juno.get('/p/:tiny/')
 def preview_url(web, tiny):
-    url = URL.find().filter(URL.tiny == tiny).all()
-    print url
-    if len(url) == 0:
-        return error('no such url')
-    if len(url) == 1:
-        return 'preview: %s' % str(url[0].address)
-    return error('somehow multiple (%s) urls with same id' % len(url))
-    
-
+    # Search the database to find the corresponding URL
+    url = URL.find().filter(URL.tiny == tiny).one()
+    return 'preview: %s' % url.address
+ 
+# Set up our redirecter
+#  - We get the short version in our variable 'tiny'
 @juno.get('/u/:tiny/')
 def get_url(web, tiny):
-    url = URL.find().filter(URL.tiny == tiny).all()
-    if len(url) == 0:
-        return error('no such url')
-    if len(url) == 1:
-        return juno.redirect(url[0].address)
-    return error('somehow multiple urls with same id')
+    url = URL.find().filter(URL.tiny == tiny).one()
+    # Redirects the user to the found address
+    juno.redirect(url.address)
 
-@juno.get('/a/:addr/')
-def add_url(web, addr):
-    # Doesn't pull in parts of url after #
-    tiny = str(hash(addr))
-    url = URL.find().filter(URL.tiny == tiny).all()
-    if len(url) != 0:
-        return juno.redirect('/p/%s/' % url[0].address)
-    if url_valid(addr): 
-        URL(address=addr, tiny=tiny).save()
-        return juno.redirect('/p/%s/' % tiny)
-    if url_valid('http://%s' % addr): 
-        URL(address='http://%s'% addr, tiny=tiny).save()
-        return juno.redirect('/p/%s/' % tiny)
-    return error('that url is not valid')
-
-
-@juno.post('/a/')
+# Set up a handler to add URLs
+@juno.route('/a/')
 def add_url_post(web):
-    addr = web.input('address')
+    addr = web.input('url')
     if not addr: juno.redirect('/')
     tiny = str(hash(addr))
-    url = URL.find().filter(URL.tiny == tiny).all()
-    if len(url) != 0:
-        return juno.redirect('/p/%s/' % url[0].address)
-    if url_valid(addr): 
-        URL(address=addr, tiny=tiny).save()
-        return juno.redirect('/p/%s/' % tiny)
-    if url_valid('http://%s' % addr): 
-        URL(address='http://%s'% addr, tiny=tiny).save()
-        return juno.redirect('/p/%s/' % tiny)
-    return error('that url is not valid')
-
-def error(msg):
-    print 'Error: %s' % msg
-    return 'Error: %s' % msg
-
-def url_valid(address):
-    host, path = urlparse.urlsplit(address)[1:3]
-    try:
-        conn = httplib.HTTPConnection(host)
-        conn.request("HEAD", path)
-        response = conn.getresponse()
-        if response.status in [404,]: return False
-    except:
-        return False
-    return True
+    url = URL.find().filter(URL.tiny == tiny).first()
+    if url is not None:
+        return juno.redirect('/p/%s/' % url.address)
+    URL(address=addr, tiny=tiny).save()
+    juno.redirect('/p/%s/' % tiny)
 
 if __name__ == '__main__': juno.run()
