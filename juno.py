670874aeec306edba1361f8f2d2cb83b0e165c86
Brian Reily <brian@brianreily.com>
2009-03-09 23:39:34 -0400

    * Added 'use_db' option again, but didn't remove imports.  Thus even if you   don't connect to a database, SQLAlchemy is still imported (as its parts   are used a lot in the model() code, and would be nontrivial to remove).

--- juno.py
+++ juno.py
@@ -49,6 +49,7 @@ class Juno(object):
                 '404_template':            '404.html',
                 '500_template':            '500.html',
                 # Database options
+                'use_db':      True,
                 'db_type':     'sqlite',
                 'db_location': ':memory:',
                 'db_models':   {},
@@ -69,7 +70,8 @@ class Juno(object):
         if self.config['use_templates']: 
             self.setup_templates()
         # Set up the database 
-        self.setup_database()
+        if self.config['use_db']:
+            self.setup_database()
 
     def setup_static(self):
         self.route(self.config['static_url'], self.config['static_handler'], '*')
